8eb898c3ba63d9376943d3d0aa601cb46c2fa88c
Osman Üngür <osmanungur@gmail.com>
2011-04-20 18:35:48 +0300

    Class for javascript interactions

--- assets/js/seven-browser.js
+++ assets/js/seven-browser.js
@@ -1,42 +1,63 @@
-var currentRepository = false;
-var currentRevision = false;
-function getRepositories() {
-    var repositoryListDiv = $("#repository-list");
+var SVNBrowser = {
+    currentRepository : false,
+	currentRevision : false,
+	ajaxUrl : 'ajax.php',
 
-    $.ajax({
-        url: 'ajax.php',
-        type: 'POST',
-        dataType: 'json',
-        data: {
-            "action" : "repositories"
-        },
-        success: function(data) {
-            if (data.length > 0) {
+	getRepositories: function(){
+	    $.ajax({
+	        url: this.ajaxUrl,
+	        type: 'POST',
+	        dataType: 'json',
+	        data: {
+	            'action' : 'repositories'
+	        },
+	        success: function(data) {
+	            if (data.length > 0) {
 
-                repositoryListDiv.html('<ul></ul>')
-                var repositoryListUl = $('#repository-list ul');
+	                $('#repository-list').html('<ul></ul>')
+	                var repositoryListUl = $('#repository-list ul');
 
-                for (var i = data.length - 1; i >= 0; i--){
-                    repositoryListUl.append('<li><a href="#" onclick="changeRepository('+ i +')">' + data[i].name + '</a><br><span class="quiet">' + data[i].url + '</span></li>')
-                }
-                
-            } else {
-                repositoryListDiv.html('No repositories found.')
-            }
-        },
-        error: function() {
-        repositoryListDiv.html('An error occured when fetching repository list.')
-        }
-    });
+	                for (var i = data.length - 1; i >= 0; i--){
+	                    repositoryListUl.append('<li><a href="#" onclick="SVNBrowser.getRepositoryLog('+ i +')">' + data[i].name + '</a><br><span class="quiet">' + data[i].url + '</span></li>')
+	                }
 
-}
+	            } else {
+	                $('#repository-list').html('No repositories found.')
+	            }
+	        },
+	        error: function() {
+	            $('#repository-list').html('An error occured when fetching repository list.')
+	        }
+	    })
+	},
 
-function getRepositoryLog() {
-    
-}
+	getRepositoryLog: function(repository_id){
+	    $.ajax({
+	        url: this.ajaxUrl,
+	        type: 'POST',
+	        dataType: 'json',
+	        data: {
+	            'action' : 'log',
+	            'repository_id' : repository_id,
+	            'limit' : $('#limit').val(),
+	            'revision-start' : $('#revision-start').val(),
+	            'revision-end' : $('#revision-end').val()
+	        },
+	        success: function(data) {
+	            if (data.length > 0) {
 
-function toggleLogOptions() {
-    $('#log-options').toggle('slow', function() {
-        // Animation complete.
-        });
+	                $('#content').empty()
+	                for (var i = data.length - 1; i >= 0; i--){
+	                    $('#content').append(data[i].revision)
+	                }
+
+	            } else {
+	                $('#content').html('No commit log found.')
+	            }
+	        },
+	        error: function() {
+	            $('#content').html('An error occured when fetching commit logs.')
+	        }
+	    })
+	}
 }
