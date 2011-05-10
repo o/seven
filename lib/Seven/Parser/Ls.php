879ce75e4acc703c378edc5d387531ffe01af11a
Osman Üngür <osmanungur@gmail.com>
2011-05-10 15:04:44 +0300

    New parser : list

---
+++ lib/Seven/Parser/Ls.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Seven\Parser;
+
+/**
+ * Ls
+ *
+ * Parser for subversion ls output
+ *
+ * @package    Seven
+ * @subpackage Seven\Parser
+ * @author     Osman Üngür <osmanungur@gmail.com>
+ * @copyright  2011 Osman Üngür
+ * @license    http://www.opensource.org/licenses/bsd-license.php BSD License
+ * @version    Version @package_version@
+ * @since      Class available since Release 1.0.0
+ * @link       http://github.com/osmanungur/seven
+ */
+class Ls extends \Seven\Parser {
+//put your code here
+}
+
+?>
