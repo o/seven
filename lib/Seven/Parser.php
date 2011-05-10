9f7b84397f6ca7d0595cd9401b0768d8c1fe9b16
Osman Üngür <osmanungur@gmail.com>
2011-05-10 15:54:26 +0300

    time2str moved to Parser

--- lib/Seven/Parser.php
+++ lib/Seven/Parser.php
@@ -55,6 +55,47 @@ abstract class Parser {
     }
 
     abstract public function parse();
+
+    /**
+     * Returns an English representation of a past date within the last month, stolen from http://ejohn.org/files/pretty.js
+     *
+     * @param string $ts
+     * @return string
+     */
+    protected function time2str($ts) {
+        if (!ctype_digit($ts))
+            $ts = strtotime($ts);
+
+        $diff = time() - $ts;
+        if ($diff == 0)
+            return 'now';
+        elseif ($diff > 0) {
+            $day_diff = floor($diff / 86400);
+            if ($day_diff == 0) {
+                if ($diff < 60)
+                    return 'just now';
+                if ($diff < 120)
+                    return '1 minute ago';
+                if ($diff < 3600)
+                    return floor($diff / 60) . ' minutes ago';
+                if ($diff < 7200)
+                    return '1 hour ago';
+                if ($diff < 86400)
+                    return floor($diff / 3600) . ' hours ago';
+            }
+            if ($day_diff == 1)
+                return 'Yesterday';
+            if ($day_diff < 7)
+                return $day_diff . ' days ago';
+            if ($day_diff < 31)
+                return ceil($day_diff / 7) . ' weeks ago';
+            if ($day_diff < 60)
+                return 'last month';
+            return date('F Y', $ts);
+        }
+        return date('F Y', $ts);
+    }    
+    
 }
 
 ?>
