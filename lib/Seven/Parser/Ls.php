86109ba04b2754cd5b583a9b5cd9ec8dde9c216f
Osman Üngür <osmanungur@gmail.com>
2011-05-10 15:54:06 +0300

    List Parser

--- lib/Seven/Parser/Ls.php
+++ lib/Seven/Parser/Ls.php
@@ -17,7 +17,49 @@ namespace Seven\Parser;
  * @link       http://github.com/osmanungur/seven
  */
 class Ls extends \Seven\Parser {
-//put your code here
+    /**
+     * XML output tags and attributes
+     */
+    const XML_ROOT_TAG = 'list';
+    const XML_ENTRY_TAG = 'entry';
+    const XML_KIND_ATTRIBUTE = 'kind';
+    const XML_NAME_TAG = 'name';
+    const XML_COMMIT_TAG = 'commit';
+    const XML_REVISION_ATTRIBUTE = 'revision';
+    const XML_AUTHOR_TAG = 'author';
+    const XML_DATE_TAG = 'date';
+    const XML_SIZE_TAG = 'size';
+    const XML_PATH_ATTRIBUTE = 'path';
+
+    /**
+     *
+     * @param string $xml
+     */
+    public function __construct($xml) {
+        $this->setXml($xml);
+        $this->load();
+    }
+
+    public function parse() {
+        if (!$this->getXml()) {
+            return false;
+        }
+        $result = new \ArrayObject();
+        if ($this->getXml()->{self::XML_ROOT_TAG}->{self::XML_ENTRY_TAG}) {
+            foreach ($this->getXml()->{self::XML_ROOT_TAG}->{self::XML_ENTRY_TAG} as $file) {
+                $result->append(new \ArrayObject(array(
+                            'kind' => (string) $file[self::XML_KIND_ATTRIBUTE],
+                            'name' => (string) $file->{self::XML_NAME_TAG},
+                            'size' => (int) $file->{self::XML_SIZE_TAG},
+                            'revision' => (int) $file->{self::XML_COMMIT_TAG}[self::XML_REVISION_ATTRIBUTE],
+                            'author' => (string) $file->{self::XML_COMMIT_TAG}->{self::XML_AUTHOR_TAG},
+                            'date' => (string) $file->{self::XML_COMMIT_TAG}->{self::XML_DATE_TAG}
+                        )));
+            }
+        }
+        return \iterator_to_array($result);
+    }
+
 }
 
 ?>
